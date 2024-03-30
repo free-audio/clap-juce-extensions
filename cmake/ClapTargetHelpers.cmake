b675d0a3290346794c21d1b41f7ca6b14074edf6
Rich Elmes <rich.elmes@a-i-media.co.uk>
2024-03-30 02:01:56 +0100

    Fix erroneous test for XCode 15.1

--- cmake/ClapTargetHelpers.cmake
+++ cmake/ClapTargetHelpers.cmake
@@ -117,7 +117,7 @@ function(clap_juce_extensions_plugin_internal)
 
         # Address the xcode linker juce issue
         # see: https://forum.juce.com/t/vst-au-builds-fail-after-upgrading-to-xcode-15/57936/43
-        if( ${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER_EQUAL "15.0.0" AND ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS "15.1")
+        if( ${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER_EQUAL "15.0.0" AND ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS "15.0.0.15000100")
             target_link_options(${claptarget} PUBLIC "-Wl,-ld_classic")
             target_compile_definitions(${claptarget} PUBLIC JUCE_SILENCE_XCODE_15_LINKER_WARNING=1)
             if (${CMAKE_OSX_DEPLOYMENT_TARGET} VERSION_LESS "10.13")
