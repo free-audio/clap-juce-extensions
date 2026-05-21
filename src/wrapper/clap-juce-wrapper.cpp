7c7241b2cbc2816d663d6c6d1433e968fc849a5e
Paul <baconpaul@users.noreply.github.com>
2026-05-21 18:59:33 -0400

    Update clap-juce-wrapper.cpp
    Make msgSize a size_t to allow strictest warning level on gcc

--- src/wrapper/clap-juce-wrapper.cpp
+++ src/wrapper/clap-juce-wrapper.cpp
@@ -1816,7 +1816,7 @@ class ClapJuceWrapper : public clap::helpers::Plugin<
                 for (auto meta : midiBuffer)
                 {
                     auto msg = meta.getMessage();
-                    auto msgSize = msg.getRawDataSize();
+                    size_t msgSize = msg.getRawDataSize();
                     if (msgSize == 2 || msgSize == 3)
                     {
                         auto evt = clap_event_midi();
